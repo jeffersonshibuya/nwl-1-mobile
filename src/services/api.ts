581756fddbeb2ffca4a41c0c1ee73f3f14f37474
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-06-08 16:12:20 -0300

    Added google api key for released app

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://192.168.0.100:3333" });
+const api = axios.create({ baseURL: "https://e-coleta-app.herokuapp.com" });
 
 export default api;
